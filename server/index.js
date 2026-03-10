90c2c468ed4af688ec4bacdace60296dd4e40970
YsbAli <ysbofficial122@gmail.com>
2026-03-10 21:32:56 +0530

    feat: added about us and fix the backend

--- server/index.js
+++ server/index.js
@@ -6,13 +6,17 @@ import router from "./routes/productRoutes.js";
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+// add router
+
+app.use('/api', router)
 
 const connectDB = async () => {
   try {
